587235c3d30e2ca1acadc2be3f657c599c8467b7
Winar Joko Alexander <winaralex417@gmail.com>
2025-10-21 20:16:02 +0700

    chores: routing

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('dashboard');
-})->name('home');
-
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
+    Route::get('/', function () {
+        return Inertia::render('dashboard');
+    })->name('home');
+
+    Route::get('/dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
