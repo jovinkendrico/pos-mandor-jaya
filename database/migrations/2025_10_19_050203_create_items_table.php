00633e163a15f1f01602dc5a2feb225c7f4a3a6c
Winar Joko Alexander <winaralex417@gmail.com>
2025-10-31 09:32:53 +0700

    fix: table migration

--- database/migrations/2025_10_19_050203_create_items_table.php
+++ database/migrations/2025_10_19_050203_create_items_table.php
@@ -14,7 +14,6 @@ return new class extends Migration {
             $table->id();
             $table->string('code')->unique();
             $table->string('name');
-            $table->foreignId('uom_id')->constrained('uoms')->cascadeOnDelete();
             $table->decimal('stock', 15, 2)->default(0);
             $table->text('description')->nullable();
             $table->timestamps();
