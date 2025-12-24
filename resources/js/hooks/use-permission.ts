2763df0af4d965aadb4058f02f66eb8109ad1244
Jovin Kendrico <jovinkendrico@gmail.com>
2025-12-24 16:36:15 +0700

    fix

--- resources/js/hooks/use-permission.ts
+++ resources/js/hooks/use-permission.ts
@@ -1,17 +1,11 @@
+import { SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
 
 export function usePermission() {
-    const { props } = usePage<{
-        auth: {
-            user: {
-                roles: string[];
-                permissions: string[];
-            };
-        };
-    }>();
+    const { props } = usePage<SharedData>();
 
-    const roles = props.auth?.user?.roles ?? [];
-    const permissions = props.auth?.user?.permissions ?? [];
+    const roles = props.auth?.roles ?? [];
+    const permissions = props.auth?.permissions ?? [];
 
     const hasRole = (role: string) => roles.includes(role);
     const hasPermission = (permission: string) => permissions.includes(permission);
